ccd0e610ffe82b766d18789f23d9ac748bcd85bb
legan100 <niconagel@outlook.de>
2023-12-06 15:53:50 +0100

    chore: Backup

--- de/legan100/api/commands/CMD_Rang.java
+++ de/legan100/api/commands/CMD_Rang.java
@@ -14,31 +14,31 @@ public class CMD_Rang implements CommandExecutor {
         YamlConfiguration cfg1 = YamlConfiguration.loadConfiguration(messageDE);
         if (sender instanceof Player p) {
             if (p.hasPermission("System.Admin")) {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.Admin"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.admin"));
             } else if (p.hasPermission("System.Dev")) {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.Dev"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.dev"));
             } else if (p.hasPermission("System.Content")) {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.Content"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.content"));
             } else if (p.hasPermission("System.SrMod")) {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.SrMod"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.srmod"));
             } else if (p.hasPermission("System.Mod")) {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.Mod"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.mod"));
             } else if (p.hasPermission("System.Sup")) {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.Sup"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.sup"));
             } else if (p.hasPermission("System.Builder")) {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.Builder"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.builder"));
             } else if (p.hasPermission("System.Techniker")) {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.Techniker"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.techniker"));
             } else if (p.hasPermission("System.Designer")) {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.Designer"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.designer"));
             } else if (p.hasPermission("System.Media")) {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.Media"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.media"));
             } else if (p.hasPermission("System.Mitarbeiter")) {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.Clan"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.mitarbeiter"));
             } else if (p.hasPermission("System.Premium")) {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.Premium"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.premium"));
             } else {
-                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.Spieler"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.rang.spieler"));
             }
         } else {
             sender.sendMessage(cfg1.getString("message.prefix") + "Du hast den Rang: §9Konsole");
--- de/legan100/api/commands/CMD_port.java
+++ de/legan100/api/commands/CMD_port.java
@@ -15,9 +15,9 @@ public class CMD_port implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
         if (sender instanceof Player p) {
             if (p.hasPermission("System.Admin") || p.hasPermission("System.Dev")) {
-                p.sendMessage(this.cfg1.getString("message.prefix") + "Server hat den Port " + p.getServer().getPort() + ".");
+                p.sendMessage(cfg1.getString("message.prefix") + "Server hat den Port " + p.getServer().getPort() + ".");
             } else {
-                p.sendMessage(this.cfg1.getString("message.noPerms"));
+                p.sendMessage(cfg1.getString("message.prefix") + cfg1.getString("message.noPerms"));
             }
         } else {
             sender.sendMessage(this.cfg1.getString("message.prefix") + "Der Server hat den Port " + sender.getServer().getPort() + ".");
